a6338babb97513b0dbec7f1e2063237c51d8af0a
olegten <olega9320@gmail.com>
2024-12-15 22:12:34 +0900

    feat(init): init commit

---
+++ backend/src/test/java/com/project/workzone/WorkzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.workzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
